8e2460aa2301637e00690fd50fe3209d447af62f
Chris-Ardrey <71410100+Chris-Ardrey@users.noreply.github.com>
2022-10-26 16:57:53 -0500

    Add files via upload

---
+++ Popup.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour
+{
+    public Canvas canvas;
+    public bool a = false;
+    public int totCount;
+    public Button myButton;
+    public GameObject theSwitch; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        if (totCount < 9)
+        {
+            totCount++;
+            a = false;
+            canvas.enabled = false;
+            myButton.enabled = true;
+            theSwitch.SetActive(true);
+        }
+        else 
+        {
+            a = true;
+            canvas.enabled = true;
+            myButton.enabled = false;
+            theSwitch.transform.Rotate(0, 0, 0);
+            theSwitch.GetComponent<BoxCollider2D>().enabled = false;
+            totCount = 0;
+        }
+    }
+}
